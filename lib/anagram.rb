41b51af5525c259f3a6578ad1042e2c95cc63e55
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-21 19:12:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word=word
+  end
+  
+  def match(arrayofpossibleanagrams)
+    newarray=[]
+    arrayofpossibleanagrams.map do |scramword|
+      if scramwordword.sort == word.sort
+        newarray<<word
+      end
+      end
+    newarray
+  end
+      
+
+end
+
